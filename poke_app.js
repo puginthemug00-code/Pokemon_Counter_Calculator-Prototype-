1d54457209e1d5013cf5f01dc88b95cfa90546ca
Derek L F <puginthemug00@gmail.com>
2026-03-20 13:40:10 -0400

    Added plan for poke_app.js
    created a skeleton for what the code will be in poke_app.js

--- poke_app.js
+++ poke_app.js
@@ -2,6 +2,23 @@
 //This file contains all the LOGIC for the type calculator.
 // It reads data from types.js and updates the HTML page.
 
+// --- state ---
+// This is the "state" of our app - the current selected attacking and defending types.
 
+
+
+// --- implement buttons for each type ---
+// when a button is clicked, update the state and recalculate the effectiveness
+
+
+
+// --- calculations for type effectiveness ---
+// pure functions that dont directly touch html
+
+
+
+
+// --- Rendering the page ---
+// update the page to show results 
 
 
